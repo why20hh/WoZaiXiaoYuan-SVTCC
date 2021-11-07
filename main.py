97dede7ad446b455ee14e258eca683fa499bf111
why20hh <63582583+why20hh@users.noreply.github.com>
2021-11-07 17:35:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,17 +4,13 @@ import json
 from urllib.parse import urlencode
 
 pushtoken = ''  # 这里填pushplus的token，可以在http://www.pushplus.plus/ 扫码注册
-user = {
-    'username': '',  # 这里填账号(手机号)
-    'password': ''  # 这里填密码
-}
+jwsession = ''  # 这里填我在校园的jwsession
 
 dk = {
     'token': pushtoken,
     'title': '我在校园每日打卡',
     'content': '',
 }
-loginUrl = "https://gw.wozaixiaoyuan.com/basicinfo/mobile/login/username"
 header = {
     "Accept-Encoding": "gzip, deflate, br",
     "Connection": "keep-alive",
@@ -29,54 +25,37 @@ dkurl = 'http://www.pushplus.plus/send'
 body = "{}"
 
 try:
-
-    def setJwsession(jwsession):
+    def main():
         header['JWSESSION'] = jwsession
-
-
-    def main(username, password):
-        url = loginUrl + "?username=" + str(user['username']) + "&password=" + str(user['password'])
-        session = requests.session()
-        response = session.post(url=url, data=body, headers=header)
-        res = json.loads(response.text)
-        if res["code"] != 0:
-            dk['content'] = '登陆失败,请检查账号信息'
+        header['Host'] = "student.wozaixiaoyuan.com"
+        header['Content-Type'] = "application/x-www-form-urlencoded"
+        url = "https://student.wozaixiaoyuan.com/health/save.json"
+        sign_data = {
+            "answers": '["0"]',
+            "latitude": "30.694365",  # 纬度
+            "longitude": "103.822602",  # 经度
+            "country": "中国",  # 国家
+            "city": "成都市",  # 城市
+            "district": "温江区",  # 区、县
+            "province": "四川省",  # 省
+            "township": "",  # 可以不填
+            "street": "",  # 可以不填
+        }
+        data = urlencode(sign_data)
+        response = requests.post(url=url, data=data, headers=header)
+        response = json.loads(response.text)
+        print(response)
+        if response["code"] == 0:
+            dk['content'] = "打卡成功"
             re = requests.post(url=dkurl, data=dk)
             print(re.text)
         else:
-            print("登陆成功")
-            jwsession = response.headers['JWSESSION']
-            setJwsession(jwsession)
-
-            header['Host'] = "student.wozaixiaoyuan.com"
-            header['Content-Type'] = "application/x-www-form-urlencoded"
-            url = "https://student.wozaixiaoyuan.com/health/save.json"
-            sign_data = {
-                "answers": '["0"]',
-                "latitude": "30.694365",  # 纬度
-                "longitude": "103.822602",  # 经度
-                "country": "中国",  # 国家
-                "city": "成都市",  # 城市
-                "district": "温江区",  # 区、县
-                "province": "四川省",  # 省
-                "township": "",  # 可以不填
-                "street": "",  # 可以不填
-            }
-            data = urlencode(sign_data)
-            response = session.post(url=url, data=data, headers=header)
-            response = json.loads(response.text)
-            print(response)
-            if response["code"] == 0:
-                dk['content'] = "打卡成功"
-                re = requests.post(url=dkurl, data=dk)
-                print(re.text)
-            else:
-                dk['content'] = "打卡失败"
-                re = requests.post(url=dkurl, data=dk)
-                print(re.text)
+            dk['content'] = "打卡失败"
+            re = requests.post(url=dkurl, data=dk)
+            print(re.text)
 
     if __name__ == '__main__':
-        main(user['username'], user['password'])
+        main()
 
 except:
     dk['content'] = '运行出错,请检查'
